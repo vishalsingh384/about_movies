5574b47c450fe5f0ae20b57794e68276c6f55c89
vishalsingh384 <vishalsing384@gmail.com>
2022-07-07 20:02:50 +0530

    :tada: started movie app

---
+++ src/Components/Banner.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { movies } from './getMovies'
+
+export default class Banner extends Component {
+  render() {
+    let movie=movies.results[0];
+    // let movie="";
+    return (
+        <>
+        {movie==""?(
+            <div className="spinner-border text-danger" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+        ):(
+            <div className='card'>
+                <img src="http://image.tmdb.org/t/p/original/6EdKBYkB1ssgGjc249ud1L55o8d.jpg" class="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">{movie. original_title}</h5>
+                    <p className="card-text">{movie.overview}</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        )}
+      </>
+    )
+  }
+}
